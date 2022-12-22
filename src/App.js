b5bed9a098b8eefa020bb1fefc7663f83b8033c3
Ginner Zapata <gzapata@vizzn.ca>
2022-12-22 06:49:30 -0300

    basic routing and gitignore

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css'
-
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Home } from "./pages/home/Home";
+import { Login } from "./pages/login/Login";
+import { Signup } from "./pages/signup/Signup";
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;

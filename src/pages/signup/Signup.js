93054612ce571e224ba7766ba817ca2ecc2f2187
Ginner Zapata <gzapata@vizzn.ca>
2022-12-22 07:10:49 -0300

    feat: created sign up page

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,6 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Signup.module.css";
 
 export const Signup = () => {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault(console.log({ displayName, password, email }));
+  };
+  return (
+    <form onSubmit={handleSubmit} className={styles["signup-form"]}>
+      <h2>Sign up</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>display name:</span>
+        <input
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <button className="btn">Sign up</button>
+    </form>
+  );
 };

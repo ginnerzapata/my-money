945c0842259ba6700b9919a1bdb1006f5b5b07d8
Ginner Zapata <gzapata@vizzn.ca>
2022-12-26 05:04:51 -0300

    feat: route guarding and authcheck on first load

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
+import { useAuthContext } from "./hooks/useAuthContext";
 import { Home } from "./pages/home/Home";
 import { Login } from "./pages/login/Login";
 import { Signup } from "./pages/signup/Signup";
 function App() {
+  const { authIsReady, user } = useAuthContext();
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-          <Route exact path="/signup">
-            <Signup />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              {user ? <Home /> : <Redirect to="/login" />}
+            </Route>
+            <Route exact path="/login">
+              {!user ? <Login /> : <Redirect to="/" />}
+            </Route>
+            <Route exact path="/signup">
+              {!user ? <Signup /> : <Redirect to="/" />}
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 }

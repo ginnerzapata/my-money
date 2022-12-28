fa2707c64977bd8fdbac6e7572ce68907dabde0b
Ginner Zapata <gzapata@vizzn.ca>
2022-12-27 23:58:10 -0300

    feat: listing transactions

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,12 +2,18 @@ import React from "react";
 import styles from "./Home.module.css";
 import { TransactionForm } from "./TransactionForm";
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCollection } from "../../hooks/useCollection";
+import { TransactionList } from "./TransactionList";
 
 export const Home = () => {
   const { user } = useAuthContext();
+  const { error, documents } = useCollection("transactions");
   return (
     <div className={styles.container}>
-      <div className={styles.content}> transaction list</div>
+      <div className={styles.content}>
+        {error && <p>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
+      </div>
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
       </div>
